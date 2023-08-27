150091062216ac23465613fd8bc5dee09c5a0615
Tasmia Rashid <143180337+S-TASMIA@users.noreply.github.com>
2023-08-27 21:22:04 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+//statement
+echo "<br>HELLO WORLD<br><br>";
+echo "I am Tasmia Rashid.
+<br>I am a student of Chittagong Mohila Polytechnic Institute.<br>
+Computer Department.Session (19-20).";
+
+//math
+$a = 100;
+$b = 20;
+echo "<br><br>Summation =".$a + $b."<br>";
+
+//concatenation
+$name = Tasmia;
+$city = Barishal;
+echo "Hello! My name is ".$name.".","I live in",".$city.";
